681f922be28454c09f4e77db2b5809a91386c990
natashta <natashta@local>
2019-04-29 00:06:41 +0300

    task done

---
+++ src/js/function.js
@@ -0,0 +1,44 @@
+export default class Team {
+  constructor() {
+    this.hero = [
+      {
+        name: 'Bowman', health: 50, level: 1, type: 'Bowman', attack: 25, defence: 25,
+      },
+      {
+        name: 'Swordsman', health: 50, level: 1, type: 'Swordsman', attack: 40, defence: 10,
+      },
+      {
+        name: 'Undead', health: 50, level: 1, type: 'Undead', attack: 10, defence: 40,
+      },
+      {
+        name: 'Magician', health: 50, level: 1, type: 'Magician', attack: 25, defence: 25,
+      },
+      {
+        name: 'Zombie', health: 50, level: 1, type: 'Zombie', attack: 40, defence: 10,
+      },
+      {
+        name: 'Daemon', health: 50, level: 1, type: 'Daemon', attack: 20, defence: 20,
+      },
+    ];
+  }
+
+  [Symbol.iterator]() {
+    const { hero } = this;
+    let current = 0;
+    return {
+      next() {
+        if (current < hero.length) {
+          const val = hero[current];
+          current += 1;
+          return {
+            done: false,
+            value: val,
+          };
+        }
+        return {
+          done: true,
+        };
+      },
+    };
+  }
+}
